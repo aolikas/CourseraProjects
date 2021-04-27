d4cdd4b0aa9bae3ba9ab4e4936dfb8db0b6be382
aolikas <46649354+aolikas@users.noreply.github.com>
2021-04-27 18:25:22 +0200

    E-legion, week2, AsyncTaskLoader

--- E-legion/AsyncTaskLoader/app/src/main/java/my/e/asynctaskloader/ContactsLoader.java
+++ E-legion/AsyncTaskLoader/app/src/main/java/my/e/asynctaskloader/ContactsLoader.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.util.Log;
-
+import android.widget.Toast;
 
 
 import java.lang.ref.WeakReference;
@@ -15,87 +15,45 @@ public class ContactsLoader extends AsyncTaskLoader<String> {
 
     public static final String ID = "id";
     public static final String TAG = "ContactsLoader";
-    private String mId;
-    private boolean isCancelled;
-    private WeakReference<Context> mContext;
+    private final String mId;
+
 
     public ContactsLoader(Context context, String id) {
         super(context);
-        mContext = new WeakReference<>(context);
-        mId = id;
+        this.mId = id;
         Log.d(TAG, "Constructor id " + mId);
     }
 
     @Override
     public String loadInBackground() {
-
-        String number = "";
-
         try {
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(2);
+            Cursor cursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                    new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND "
+                            + ContactsContract.CommonDataKinds.Phone.TYPE + " = ?",
+                    new String[]{mId, String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)},
+                    null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                cursor.close();
+
+                return number;
+            }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        Context context = mContext.get();
-        if (context == null) return null;
-
-        Cursor cursor = getContext().getContentResolver()
-                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
-                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND "
-                                + ContactsContract.CommonDataKinds.Phone.TYPE + " = ?",
-                        new String[]{mId, String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)},
-                        null);
-
-        if (cursor != null && cursor.moveToFirst()) {
-            number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            cursor.close();
-        }
-
-        return number;
+        return null;
     }
 
-    /**
-     public void setId(String id) {
-     mId = id;
-     }
-     */
-
-    public boolean isCancelled() {
-        return isCancelled;
-    }
-
-
-    @Override
-    protected void onStartLoading() {
-        isCancelled = false;
-        if(mId == null) return;
-        forceLoad();
-    }
-
-    @Override
-    protected void onReset() {
-        super.onReset();
-        isCancelled = false;
-    }
-
-    @Override
-    protected void onStopLoading() {
-        super.onStopLoading();
-        cancelLoad();
-    }
 
     @Override
     public void onCanceled(String data) {
         super.onCanceled(data);
-        isCancelled = true;
-        deliverResult(null);
+        Toast.makeText(getContext(),
+                "Cancelled", Toast.LENGTH_SHORT).show();
     }
-
-    @Override
-    public void deliverResult(String data) {
-        super.deliverResult(data);
-    }
-
 }
